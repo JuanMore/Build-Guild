2e06b0df223977c81b48cb0790e0302be2b2a989
Juan <morenostudent1@gmail.com>
2021-11-24 13:44:52 -0800

    incorp auth

--- routes/auth.js
+++ routes/auth.js
@@ -1,10 +1,47 @@
 const express = require('express')
 const router = express.Router()
 const User = require('../models/user')
-
+const catchErr = require('../helpers/wrapAsync')
+const passport = require('passport')
 
 router.get('/register', (req, res) => {
     res.render('auth/register')
+})
+router.post('/register', catchErr(async (req, res) => {
+    try {
+        const {
+            email,
+            username,
+            password
+        } = req.body
+        const user = new User({
+            email,
+            username
+        })
+        const newUser = await User.register(user, password)
+        req.flash('success', `Welcome to Build Guild, ${user.username}.`)
+        res.redirect('/pages')
+    } catch (e) {
+        req.flash('error', e.message)
+        res.redirect('register')
+    }
+}))
+
+
+router.get('/login', (req, res) => {
+    res.render('auth/login')
+})
+
+
+router.post('/login', passport.authenticate('local', {
+    // passport auth - flash if failure
+    failureFlash: true,
+    // redirect
+    failureRedirect: '/login'
+    // if authenticated
+}), (req, res) => {
+    req.flash('success', `Welcome back!`)
+    res.redirect('/pages')
 })
 
 module.exports = router
