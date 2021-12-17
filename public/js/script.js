e931a236c6ef51dbbae21bb9b8987d1ecb2ffe86
Juan <morenostudent1@gmail.com>
2021-12-17 11:01:14 -0800

    quick updates

--- public/js/script.js
+++ public/js/script.js
@@ -25,3 +25,17 @@ function backToTop() {
     document.body.scrollTop = 0;
     document.documentElement.scrollTop = 0;
 }
+
+// Like button 
+let likes = 0;
+document.getElementById('like-button')
+const likeBtn = document.getElementById('like-button')
+
+document.getElementById('likes').innerHTML = likes
+likeBtn.onclick = () => {
+    likes += 1
+    likeBtn.innerHTML = likes +
+    `<a href="#" id="like-button" class="d-inline-block text-muted ml-2">
+    <strong id="likes"></strong> <i class="fas fa-thumbs-up"></i></small>
+    </a>`
+}
